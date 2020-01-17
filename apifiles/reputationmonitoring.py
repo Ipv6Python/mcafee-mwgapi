4fd608d29b51953bb94aa859cd13acf768d4e7db
Ipv6Python <davuluri4u2@gmail.com>
2020-01-16 22:05:17 -0600

    Basic API functionalities.

---
+++ apifiles/reputationmonitoring.py
@@ -0,0 +1,9 @@
+"""
+Trustedsource
+https://wordtothewise.com/2010/09/reputation-monitoring-sites/
+https://urlfiltering.paloaltonetworks.com/
+https://zeltser.com/lookup-malicious-websites/
+
+"""
+
+
---
+++ apifiles/subscribedlists.py
@@ -0,0 +1,6 @@
+"""
+Microsoft provided IP/URL ranges
+AWS provided IP/URL ranges
+Citrix provided IP/URL ranges
+
+"""
